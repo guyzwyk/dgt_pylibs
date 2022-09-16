ae4af4e55a17544600865345152560efa2dbde9b
Guy Robert NDZOMO ZINGUI <guyzwyk@gmail.com>
2022-09-16 09:56:57 +0100

    Modificationd de la bibliotheque

--- dgt_includes.py
+++ dgt_includes.py
@@ -13,8 +13,10 @@ def splashCenter(appRef, width, height):
     return toRet
 
 class digiForm:
-    def __init__(self, appRef):
+    def __init__(self, appRef, formBg="#326273", form_txtColor="#fff"):
         self.appRef = appRef
+        self.formBg = formBg
+        self.form_txtColor = form_txtColor
 
     def get_digiForm(self):
         return self.appRef
@@ -23,17 +25,37 @@ class digiForm:
         self.appRef = newAppRef
         return self.appRef
 
-    def dgt_formLabel(self, labelTxt, labelFontType, labelFontSize, labelGridRow, labelGridCol, stickyPosition):
-        toRet       =   Label(self, text=labelTxt, font=(labelFontType, labelFontSize))
-        toRet.grid(row=labelGridRow, column=labelGridCol, sticky=stickyPosition)
+    def set_formBgColor(self, new_bgColor):
+        self.formBg = new_bgColor
+        return self.formBg
+
+    def set_formForeColor(self, new_foreColor):
+        self.form_txtColor = new_foreColor
+        return self.form_txtColor
+
+    def dgt_formLabel(self, labelTxt, labelFontDescription, labelBg="#000", labelFg="#000"):
+        #labelFontDescription = ('fontType, fontSize') ex: "arial 14"
+        toRet       =   Label(self, text=labelTxt, font=labelFontDescription, bg=labelBg, fg=labelFg)
+        #toRet.grid(row=labelGridRow, column=labelGridCol, sticky=stickyPosition)
         #font=('bold', 14)
         #stickyPosition(W/E)
+        return toRet
+    
+    def dgt_formInput(self, frm_textVariable, inputWidth=15, inputBorder=2, input_fontSize=10):
+        #toRet       =   StringVar()
+        frmEntry    =   Entry(self, textvariable=frm_textVariable, width=inputWidth, bd=inputBorder, font=input_fontSize)
+        return frmEntry
 
-    def dgt_formElement(self, frmEltGridRow, frmEltGridCol, stickyPosition):
-        toRet       =   StringVar()
-        frmEntry    =   Entry(self, textvariable=toRet)
-        frmEntry.grid(row=frmEltGridRow, column=frmEltGridCol, sticky=stickyPosition)
+    def dgt_formButton(self, varCmd, btnTxt="Button", btnWidth=10, btnBg="#000", btnFg="#000"):
+        newBtn      =   Button(self, text=btnTxt, width=btnWidth, command=varCmd, bg=btnBg, fg=btnFg)
+        return newBtn
 
-    def dgt_formButton(self, btnTxt, btnWidth, varCmd, btnGridRow, btnGridCol, btnPadY):
-        newBtn      =   Button(self, text=btnTxt, width=btnWidth, command=varCmd)
-        newBtn.grid(row=btnGridRow, column=btnGridCol, pady=btnPadY)
+    def dgt_formElement(self, frm_textVariable, width=35, bd=2, font=10):
+        #toRet       =   StringVar()
+        frmEntry    =   Entry(self, textvariable=frm_textVariable)
+        return frmEntry
+
+
+myForm = digiForm("MyRef", "BgColor=blue", "ForeColor=white")
+
+print(myForm)
